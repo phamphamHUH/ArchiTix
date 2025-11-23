c8c8b1ba53665ada2f0f4bc1ba4a03a6ff470af5
Marc Jodel <marcjodeldelacruz@gmail.com>
2025-11-23 18:22:27 +0800

    done na sa sell

--- js/buy.js
+++ js/buy.js
@@ -1,3 +1,17 @@
+// ADDS FOCUS FEATURE WHEN ASSIGNED ICON IS CLICKED
+const searchInput = document.getElementById("search-input");
+const searchIcon = document.querySelector(".search-icon");
+const filterInput = document.getElementById("event-filter");
+const filterIcon = document.querySelector(".filter-icon");
+
+searchIcon.addEventListener("click", () => {
+  searchInput.focus();
+});
+
+filterIcon.addEventListener("click", () => {
+  filterInput.focus();
+});
+
 const events = [
   {
     name: "Twice",
@@ -124,6 +138,120 @@ events.forEach((e) => {
   }
 });
 
-function filter() {}
+const filterValue = document.getElementById("event-filter");
 
-function search() {}
+filterValue.addEventListener("change", () => {
+  filter(filterValue.value);
+});
+
+function filter(filterCriteria) {
+  showingContainer.innerHTML = "";
+  upcomingContainer.innerHTML = "";
+
+  let sortedEvents = [...events];
+
+  switch (filterCriteria) {
+    case "Price - Low to High":
+      sortedEvents.sort((a, b) => {
+        const aMin = parseInt(
+          a.price
+            .replace(/[^0-9]/g, "")
+            .split("")
+            .slice(0, 4)
+            .join("")
+        );
+        const bMin = parseInt(
+          b.price
+            .replace(/[^0-9]/g, "")
+            .split("")
+            .slice(0, 4)
+            .join("")
+        );
+        return aMin - bMin;
+      });
+      break;
+    case "Price - High to Low":
+      sortedEvents.sort((a, b) => {
+        const aMin = parseInt(
+          a.price
+            .replace(/[^0-9]/g, "")
+            .split("")
+            .slice(0, 4)
+            .join("")
+        );
+        const bMin = parseInt(
+          b.price
+            .replace(/[^0-9]/g, "")
+            .split("")
+            .slice(0, 4)
+            .join("")
+        );
+        return bMin - aMin;
+      });
+      break;
+    case "Newest First":
+      sortedEvents.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+      break;
+    case "Oldest First":
+      sortedEvents.sort((a, b) => new Date(a.date) - new Date(b.date));
+      break;
+  }
+
+  sortedEvents.forEach((e) => {
+    const card = document.createElement("div");
+    card.classList.add("event");
+    card.innerHTML = `
+      <img src="${e.img}" alt="Event Image" class="image"/>
+      <div class="eventDetails">
+        <h3>${e.name}</h3>
+        <h5>Event's Place: ${e.place}</h5>
+        <h5>Price: ${e.price}</h5>
+        <h5>Date: ${e.date}</h5>
+        <button>
+          <div><img src="../../assets/icons/seat.png" alt="Seat Icon" /></div>
+          View Seats
+        </button>
+      </div>
+    `;
+    if (e.status === "upcoming") upcomingContainer.appendChild(card);
+    else showingContainer.appendChild(card);
+  });
+}
+
+const searchValue = document.getElementById("search-input");
+
+searchValue.addEventListener("input", () => {
+  search(searchValue.value);
+});
+
+function search(searchValue) {
+  showingContainer.innerHTML = "";
+  upcomingContainer.innerHTML = "";
+
+  const filteredEvents = events.filter(
+    (e) =>
+      e.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+      e.place.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  filteredEvents.forEach((e) => {
+    const card = document.createElement("div");
+    card.classList.add("event");
+    card.innerHTML = `
+      <img src="${e.img}" alt="Event Image" class="image"/>
+      <div class="eventDetails">
+        <h3>${e.name}</h3>
+        <h5>Event's Place: ${e.place}</h5>
+        <h5>Price: ${e.price}</h5>
+        <h5>Date: ${e.date}</h5>
+        <button>
+          <div><img src="../../assets/icons/seat.png" alt="Seat Icon" /></div>
+          View Seats
+        </button>
+      </div>
+    `;
+    if (e.status === "upcoming") upcomingContainer.appendChild(card);
+    else showingContainer.appendChild(card);
+  });
+}
