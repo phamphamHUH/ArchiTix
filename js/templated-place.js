45958be41a0377242f1e7f8edb0d6b991d7abe95
Marc Jodel <marcjodeldelacruz@gmail.com>
2025-11-28 14:59:38 +0800

    template

--- js/templated-place.js
+++ js/templated-place.js
@@ -1,39 +1,39 @@
 const contents = [
   {
-    src: "../../assets/images/placeholder.png",
-    name: "Araneta",
-    location: "Cubao",
+    src: "../../assets/images/StaRosaSportsComplex.png",
+    name: "Sta. Rosa Sports Complex",
+    location: "Sta. Rosa, Laguna",
     rate: "10%",
   },
   {
-    src: "../../assets/images/placeholder.png",
-    name: "Araneta",
-    location: "Cubao",
-    rate: "100%",
+    src: "../../assets/images/FilOilEcoilCentre.png",
+    name: "FilOil EcoOil Centre",
+    location: "San Juan, Metro Manila",
+    rate: "15%",
   },
   {
-    src: "../../assets/images/placeholder.png",
-    name: "Araneta",
-    location: "Cubao",
-    rate: "10%",
+    src: "../../assets/images/CandonCityArena.png",
+    name: "Candon City Arena",
+    location: "Candon, Ilocos Sur",
+    rate: "12%",
   },
   {
-    src: "../../assets/images/placeholder.png",
-    name: "Araneta",
-    location: "Cubao",
-    rate: "100%",
+    src: "../../assets/images/7.png",
+    name: "Newport Performing Arts Theatre",
+    location: "Pasay City",
+    rate: "13%",
   },
   {
-    src: "../../assets/images/placeholder.png",
-    name: "Araneta",
-    location: "Cubao",
-    rate: "10%",
+    src: "../../assets/images/DL.png",
+    name: "DL Umali Auditorium",
+    location: "Los Baños, Laguna",
+    rate: "14%",
   },
   {
-    src: "../../assets/images/placeholder.png",
-    name: "Araneta",
-    location: "Cubao",
-    rate: "100%",
+    src: "../../assets/images/CGF.png",
+    name: "CCF Center Auditorium",
+    location: "Ortigas, Pasig City",
+    rate: "11%",
   },
 ];
 
@@ -42,20 +42,19 @@ const container = document.getElementById("event-places");
 contents.forEach((place) => {
   const card = document.createElement("div");
   card.classList.add("event");
-  const img = document.createElement("img");
-  img.src = place.src;
-  img.alt = "Event Place Picture";
-  card.appendChild(img);
-  const eventDetails = document.createElement("div");
-  const h3 = document.createElement("h3");
-  const location = document.createElement("h5");
-  const price = document.createElement("h5");
-  h3.textContent = `Venue: ${place.name}`;
-  location.textContent = `Location: ${place.location}`;
-  price.textContent = `Commission: ${place.rate}`;
-  eventDetails.appendChild(h3);
-  eventDetails.appendChild(location);
-  eventDetails.appendChild(price);
-  card.appendChild(eventDetails);
+  card.innerHTML = `
+    <img src="${place.img}" alt="Event Image" class="image"/>
+    <div class="eventDetails">
+      <h3>${place.name}</h3>
+      <h5>Location: ${place.place}</h5>
+      <h5>Price: ${place.price}</h5>
+        <a href="../../edit-event.html">
+          <button>
+            <div><img src="../../assets/images/edit.png" alt="Edit Icon" /></div>
+            Edit Venue
+          </button>
+        </a>
+    </div>
+ `;
   container.appendChild(card);
 });
